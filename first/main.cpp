598a6c7c4c12616e2c8a4dbbec814a536cff5d90
Vykintas <vdaugintis9@gmail.com>
2026-02-25 19:12:01 +0200

    first5

--- first/main.cpp
+++ first/main.cpp
@@ -26,14 +26,39 @@ using namespace std;
 // }
 
 //3
+// int main() {
+//     string Marke = "Nissan";
+//     string Modelis = "Juke";
+//     int Pagaminimo_metai = 2010;
+//     double Litrazas = 1.4;
+//     string Spalva = "juodos";
+//
+//     cout << "Automobilis " << Marke<<" "<<Modelis << " yra pagamintas "<<Pagaminimo_metai<<" metais. Jo motoras "<<Litrazas<<" litrazo. Automobilis yra "<<Spalva<<" spalvos "<< endl;
+//     return 0;
+//
+// }
+//4
+// int main() {
+//
+//     int a = 13;
+//     int b = 5;
+//     double c = 17.5;
+//
+//     cout << a+ b - c<< endl;
+//     cout << 15/2 +c <<endl;
+//     cout << a /static_cast<double>(b) +2*c <<endl;
+//     cout << 14%3+6.3 + b/a << endl;
+//     cout << static_cast<int>(c)%5+a-b << endl;
+//     cout << 13.5/2 +4.0*3.5 +18 << endl;
+//     return 0;
+// }
+//5
 int main() {
-    string Marke = "Nissan";
-    string Modelis = "Juke";
-    int Pagaminimo_metai = 2010;
-    double Litrazas = 1.4;
-    string Spalva = "juodos";
-
-    cout << "Automobilis " << Marke<<" "<<Modelis << " yra pagamintas "<<Pagaminimo_metai<<" metais. Jo motoras "<<Litrazas<<" litrazo. Automobilis yra "<<Spalva<<" spalvos "<< endl;
+    double a,b,c,d,e;
+    double vidurkis;
+    cout << " Iveskite 5 skaicius: ";
+    cin >> a>>b>>c>>d>>e;
+    vidurkis = (a+b+c+d+e)/5;
+    cout <<"Ivestu skaiciu vidurkis: "<<vidurkis<<endl;
     return 0;
-
 }
