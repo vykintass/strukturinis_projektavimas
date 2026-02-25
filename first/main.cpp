f16680de11431423de9dab48e9bb16e7182e88df
Vykintas <vdaugintis9@gmail.com>
2026-02-25 11:01:14 +0200

    first3

--- first/main.cpp
+++ first/main.cpp
@@ -1,17 +1,39 @@
-#include <iostream>// ut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-
+#include <iostream>
+#include <string>
 using namespace std;
 //1
+// int main() {
+//     string Vardas = "Jonas";
+//     string Pavarde = "Jonaitis";
+//     int Amzius = 22;
+//     string Grupe = "IS25SN";
+//     int Kursas = 1;
+//     string Studiju_programos_pavadinimas = "Programu sistemos ir sauga";
+//     cout << Vardas << " " << Pavarde +" jam yra " << Amzius <<" metai, jis mokosi " << Grupe+" grupeje, "<<Kursas<<" kurse ir jo studijos vadinasi "<<Studiju_programos_pavadinimas;
+//     return 0;//
+// }
+//2
+// int main() {
+//     string Komandos_pavadinimas = "Kauno Zalgiris";
+//     int Ikurimo_metai = 1944;
+//     string Savininkas = "Jonas Okmanas";
+//     string Arena = "Zalgirio arena";
+//     int Vietu_skaicius = 15500;
+//
+//     cout << "Krepsinio klubas " << Komandos_pavadinimas << " ikurtas "<<Ikurimo_metai<<", komandos savininkas "<<Savininkas<<", komandos arenos pavadinimas "<< Arena<<", kuri talpina "<<Vietu_skaicius<<" ziurovu"<< endl;
+//     return 0;
+//
+// }
+
+//3
 int main() {
-    string Vardas = "Jonas";
-    string Pavarde = "Jonaitis";
-    int Amzius = 22;
-    string Grupe = "IS25SN";
-    int Kursas = 1;
-    string Studiju_programos_pavadinimas = "Programu sistemos ir sauga";
+    string Marke = "Nissan";
+    string Modelis = "Juke";
+    int Pagaminimo_metai = 2010;
+    double Litrazas = 1.4;
+    string Spalva = "juodos";
 
-    cout << Vardas << " " << Pavarde +" jam yra " << Amzius <<" metai, jis mokosi " << Grupe+" grupeje, "<<Kursas<<" kurse ir jo studijos vadinasi "<<Studiju_programos_pavadinimas;
+    cout << "Automobilis " << Marke<<" "<<Modelis << " yra pagamintas "<<Pagaminimo_metai<<" metais. Jo motoras "<<Litrazas<<" litrazo. Automobilis yra "<<Spalva<<" spalvos "<< endl;
+    return 0;
 
-
-    return 0;// TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
