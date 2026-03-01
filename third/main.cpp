328613e3486de49d3a8ec4ae5360de5712b8573c
Vykintas <vdaugintis9@gmail.com>
2026-03-01 18:50:51 +0200

    masyvai-1

---
+++ third/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int masyvas[5];
+    int n = 5;
+    int suma = 0;
+
+    cout << "Iveskite masyvo elementus, tinka tik sveikieji skaiciai: \n";
+
+    for (int i = 0; i < n; i++) {
+        cout << "Iveskite " << i + 1 << " -aji masyvo elementa: ";
+        cin >> masyvas[i];
+        suma += masyvas[i];
+    }
+    cout << "Masyvo elementu suma lygi: " << suma << "\n";
+    int maziausias = masyvas[0];
+    int didziausias = masyvas[0];
+    for (int i = 0; i < n; i++) {
+        if (masyvas[i] < maziausias)
+            maziausias = masyvas[i];
+
+        if (masyvas[i] > didziausias)
+            didziausias = masyvas[i];
+    }
+    cout << "Didziausias masyvo skaicius: " << didziausias << "\n";
+    cout << "Maziausias masyvo skaicius: " << maziausias << "\n";
+    return 0;
+}
