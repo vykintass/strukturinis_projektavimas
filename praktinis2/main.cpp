3a38586bd95c11dd0f490b1af3ea6471f4ed1cb2
Vykintas <vdaugintis9@gmail.com>
2026-03-05 09:59:12 +0200

    tikrinimai prideti

--- praktinis2/main.cpp
+++ praktinis2/main.cpp
@@ -3,35 +3,49 @@
 using namespace std;
 
 int main() {
-    const int MOKINIAI = 3;
-    const int PAZYMIAI = 3;
+    const int MOKINIAI = 100;
+    const int PAZYMIAI = 10;
     string mokiniai[MOKINIAI] = {};
     int pazymiai[MOKINIAI][PAZYMIAI] = {};
     int kiek_pazymiu;
+    int kiek_mokiniu;
 
-    for (int i = 0; i < MOKINIAI; i++) {
-        cout << "Iveskite mokinio varda: ";
-        cin >> mokiniai[i];
-        cout << "Kiek pazymiu turi " << mokiniai[i] << " ?";
-        cin >> kiek_pazymiu;
-        if (0 <= kiek_pazymiu && kiek_pazymiu <= PAZYMIAI) {
-            for (int j = 0; j < kiek_pazymiu; j++) {
-                cout << "Iveskite " << j + 1 << " mokinio pazymi: " << endl;
-                cin >> pazymiai[i][j];
+    cout << "Iveskite mokiniu skaiciu (iki 100)? ";
+    cin >> kiek_mokiniu;
+    if (kiek_mokiniu > 0 && kiek_mokiniu <= MOKINIAI) {
+        for (int i = 0; i < kiek_mokiniu; i++) {
+            cout << "Iveskite mokinio varda: ";
+            cin >> mokiniai[i];
+            cout << "Kiek pazymiu turi " << mokiniai[i] << " ?";
+            cin >> kiek_pazymiu;
+            if (0 <= kiek_pazymiu && kiek_pazymiu <= PAZYMIAI) {
+                for (int j = 0; j < kiek_pazymiu; j++) {
+                    cout << "Iveskite " << j + 1 << " mokinio pazymi: " << endl;
+                    int pazymys;
+                    cin >> pazymys;
+                    while (pazymys < 1 || pazymys > 10) {
+                        cout << "Netinkamas pazymys, Iveskite dar karta: " << endl;
+                        cin >> pazymys;
+                    }
+                    pazymiai[i][j] = pazymys;
+                }
+            } else {
+                cout << "Netinkamas pazymiu skaicius: " << endl;
+                return 0;
             }
-        } else {
-            cout << "Netinkamas pazymiu skaicius: " << endl;
-            return 0;
         }
-    }
-    for (int j = 0; j < MOKINIAI; j++) {
-        cout << mokiniai[j] << " " << endl;
-        for (int k = 0; k < PAZYMIAI; k++) {
-            if (pazymiai[j][k] == 0) {
-                break;
+        for (int j = 0; j < kiek_mokiniu; j++) {
+            cout << mokiniai[j] << " :";
+            for (int k = 0; k < PAZYMIAI; k++) {
+                if (pazymiai[j][k] == 0) {
+                    break;
+                }
+                cout << pazymiai[j][k] << " ";
             }
-            cout << pazymiai[j][k] << " ";
+            cout << endl;
         }
+    } else {
+        cout << "Skaicius negali buti lygus nuliui, neigiamas, daugiau negu 100. " << endl;
     }
 
 
